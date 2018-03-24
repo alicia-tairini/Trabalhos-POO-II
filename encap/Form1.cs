f9506c48923f08b4af954bc82f04e29617cd3ecb
AliciaTairini <allicyatairiny@hotmail.com>
2018-03-24 15:35:10 -0300

    add encapsulamento

---
+++ encap/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace encap
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            Pessoa obj = new Pessoa();
+            obj.Nome = txtNome.Text;
+            obj.Idade = Convert.ToInt32(txtIdade.Text);
+            obj.Sexo = txtSexo.Text;
+            MessageBox.Show(obj.Nome + " Possui " + obj.Idade.ToString() + " Anos e é do sexo" + obj.Sexo);
+
+        
+        }
+    }
+}
