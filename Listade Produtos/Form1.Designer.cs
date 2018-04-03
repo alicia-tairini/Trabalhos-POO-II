5fd1de03cd477bcc7e389ac261939ccc7ea14ac4
AliciaTairini <allicyatairiny@hotmail.com>
2018-04-03 16:32:20 -0300

    Produtos

---
+++ Listade Produtos/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace Listade_Produtos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listProdutos = new System.Windows.Forms.ListBox();
+            this.btnAdicionar = new System.Windows.Forms.Button();
+            this.btnRemvTudo = new System.Windows.Forms.Button();
+            this.btnRemover = new System.Windows.Forms.Button();
+            this.btbCancelar = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.txtItem = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblData = new System.Windows.Forms.Label();
+            this.lblHora = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Thistle;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Modern No. 20", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(91, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(205, 38);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "PRODUTOS";
+            // 
+            // listProdutos
+            // 
+            this.listProdutos.BackColor = System.Drawing.Color.Thistle;
+            this.listProdutos.FormattingEnabled = true;
+            this.listProdutos.Location = new System.Drawing.Point(120, 62);
+            this.listProdutos.Name = "listProdutos";
+            this.listProdutos.Size = new System.Drawing.Size(147, 173);
+            this.listProdutos.Sorted = true;
+            this.listProdutos.TabIndex = 2;
+            // 
+            // btnAdicionar
+            // 
+            this.btnAdicionar.BackColor = System.Drawing.Color.Plum;
+            this.btnAdicionar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAdicionar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdicionar.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnAdicionar.Location = new System.Drawing.Point(43, 303);
+            this.btnAdicionar.Name = "btnAdicionar";
+            this.btnAdicionar.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.btnAdicionar.Size = new System.Drawing.Size(92, 46);
+            this.btnAdicionar.TabIndex = 3;
+            this.btnAdicionar.Text = "Adicionar Iten";
+            this.btnAdicionar.UseVisualStyleBackColor = false;
+            this.btnAdicionar.Click += new System.EventHandler(this.btnAdicionar_Click);
+            // 
+            // btnRemvTudo
+            // 
+            this.btnRemvTudo.BackColor = System.Drawing.Color.Plum;
+            this.btnRemvTudo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRemvTudo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRemvTudo.Location = new System.Drawing.Point(43, 375);
+            this.btnRemvTudo.Name = "btnRemvTudo";
+            this.btnRemvTudo.Size = new System.Drawing.Size(92, 45);
+            this.btnRemvTudo.TabIndex = 4;
+            this.btnRemvTudo.Text = "Remover Tudo";
+            this.btnRemvTudo.UseVisualStyleBackColor = false;
+            this.btnRemvTudo.Click += new System.EventHandler(this.btnRemvTudo_Click);
+            // 
+            // btnRemover
+            // 
+            this.btnRemover.BackColor = System.Drawing.Color.Plum;
+            this.btnRemover.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btnRemover.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRemover.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRemover.Location = new System.Drawing.Point(176, 303);
+            this.btnRemover.Margin = new System.Windows.Forms.Padding(3, 3, 3, 9);
+            this.btnRemover.Name = "btnRemover";
+            this.btnRemover.Size = new System.Drawing.Size(91, 46);
+            this.btnRemover.TabIndex = 5;
+            this.btnRemover.Text = "Remover Item";
+            this.btnRemover.UseVisualStyleBackColor = false;
+            this.btnRemover.Click += new System.EventHandler(this.btnRemover_Click);
+            // 
+            // btbCancelar
+            // 
+            this.btbCancelar.BackColor = System.Drawing.Color.Plum;
+            this.btbCancelar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btbCancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btbCancelar.Location = new System.Drawing.Point(176, 375);
+            this.btbCancelar.Name = "btbCancelar";
+            this.btbCancelar.Size = new System.Drawing.Size(91, 44);
+            this.btbCancelar.TabIndex = 6;
+            this.btbCancelar.Text = "Cancelar";
+            this.btbCancelar.UseVisualStyleBackColor = false;
+            this.btbCancelar.Click += new System.EventHandler(this.btbCancelar_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.BackColor = System.Drawing.Color.Plum;
+            this.btnSair.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnSair.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSair.Location = new System.Drawing.Point(301, 348);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(85, 35);
+            this.btnSair.TabIndex = 7;
+            this.btnSair.Text = "SAIR";
+            this.btnSair.UseVisualStyleBackColor = false;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // txtItem
+            // 
+            this.txtItem.BackColor = System.Drawing.Color.Thistle;
+            this.txtItem.Location = new System.Drawing.Point(170, 264);
+            this.txtItem.Name = "txtItem";
+            this.txtItem.Size = new System.Drawing.Size(216, 20);
+            this.txtItem.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Plum;
+            this.label2.Font = new System.Drawing.Font("Montserrat", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(7, 268);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(157, 16);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Produtos para lista:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // lblData
+            // 
+            this.lblData.AutoSize = true;
+            this.lblData.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblData.Location = new System.Drawing.Point(233, 428);
+            this.lblData.Name = "lblData";
+            this.lblData.Size = new System.Drawing.Size(34, 13);
+            this.lblData.TabIndex = 10;
+            this.lblData.Text = "Data";
+            // 
+            // lblHora
+            // 
+            this.lblHora.AccessibleRole = System.Windows.Forms.AccessibleRole.ComboBox;
+            this.lblHora.AutoSize = true;
+            this.lblHora.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHora.Location = new System.Drawing.Point(325, 428);
+            this.lblHora.Name = "lblHora";
+            this.lblHora.Size = new System.Drawing.Size(34, 13);
+            this.lblHora.TabIndex = 11;
+            this.lblHora.Text = "Hora";
+            this.lblHora.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Orchid;
+            this.ClientSize = new System.Drawing.Size(399, 450);
+            this.Controls.Add(this.lblHora);
+            this.Controls.Add(this.lblData);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtItem);
+            this.Controls.Add(this.btnRemvTudo);
+            this.Controls.Add(this.btnAdicionar);
+            this.Controls.Add(this.btnRemover);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btbCancelar);
+            this.Controls.Add(this.listProdutos);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listProdutos;
+        private System.Windows.Forms.Button btnAdicionar;
+        private System.Windows.Forms.Button btnRemvTudo;
+        private System.Windows.Forms.Button btnRemover;
+        private System.Windows.Forms.Button btbCancelar;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.TextBox txtItem;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblData;
+        private System.Windows.Forms.Label lblHora;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Listade Produtos/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Listade_Produtos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        #region Botão Adicionar
+        private void btnAdicionar_Click(object sender, EventArgs e)
+        {
+            string nome = txtItem.Text;
+            listProdutos.Items.Add(nome.ToUpper());
+            txtItem.Clear();
+            txtItem.Focus();
+        }
+        #endregion
+
+        #region Botão Remover
+        private void btnRemover_Click(object sender, EventArgs e)
+        {
+            int valor = listProdutos.SelectedIndex;
+            if (valor == -1)
+            {
+                MessageBox.Show("Selecione um icone para exluir!", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            else
+            {
+                listProdutos.Items.RemoveAt(valor);
+            }
+        }
+        #endregion
+
+        #region Botão Remover Tudo
+        private void btnRemvTudo_Click(object sender, EventArgs e)
+        {
+            if (listProdutos.Items.Count == 0)
+            {
+                MessageBox.Show("A lista esta vazia", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            else
+            {
+                listProdutos.Items.Clear();
+                txtItem.Focus();
+            }
+
+        }
+        #endregion
+
+        #region Botão cancelar
+        private void btbCancelar_Click(object sender, EventArgs e)
+        {
+            txtItem.Clear();
+            txtItem.Focus();
+        }
+        #endregion
+
+        #region Botão sair
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Tem certeza que deseja sair?", "Finalizar", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                Application.Exit();
+            }  
+        }
+
+        #endregion
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblData.Text = DateTime.Now.ToString("dd/MM/yyy");
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lblHora.Text = DateTime.Now.ToString("HH:mm:ss");
+        }
+    }
+
+}
